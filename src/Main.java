a83353ed1b590e333911240a7e8457c8d7e4ff7b
denisinside <d.shvachka@ukma.edu.ua>
2023-03-23 01:04:09 +0200

    made meow

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    static void meow(){
+        System.out.println("meow meow bitch");
+    }
 }
