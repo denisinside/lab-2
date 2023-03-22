5e02f2a134885059928e61ee5faa491a410e4024
TimMatthew <123355026+TimMatthew@users.noreply.github.com>
2023-03-22 23:19:33 +0000

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int a = 5;
-        System.out.println(a);
+        System.out.println("Свабоду чєсним варам!");
     }
 
     static void meow(){
