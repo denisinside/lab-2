f9091a16361de854a4d5e763fd2a4545ee2e761f
Foltest of Temeria <tymophiy05@hmail.com>
2023-04-05 13:35:38 +0300

    Додав Фрейм головного меню

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Свабоду чєсним варам!");
-    }
-
-    static void meow(){
-        System.out.println("meow meow bitch");
+        JFrame jf = new JFrame();
+        jf.setSize(200,200);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setVisible(true);
     }
 }
