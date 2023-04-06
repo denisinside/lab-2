c265f321d8e44ea8a806c44c8d67f2bc92c39ab5
TimMatthew <t.mattw.0000@gmail.com>
2023-04-06 17:32:57 +0300

    Додав панельку меню

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,19 +1,31 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class MainMenu {
     protected static void setMainFrame(){
-        JFrame jf = new JFrame();
-        JPanel jp = new JPanel(new FlowLayout());
-        jf.setSize(500,500);
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jf.setVisible(true);
-        JLabel jl1 = new JLabel("Hello world!");
-        JLabel jl2 = new JLabel("Свабоду чєсним варам!");
+        JFrame mainFrame = new JFrame();
+        JPanel buttonPanel = new JPanel(new GridLayout(6,0));
 
-        jp.add(jl1);
-        jp.add(jl2);
+        mainFrame.setSize(500,500);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        jf.add(jp);
+        JLabel mainHint = new JLabel("STORE SIMULATOR");
+        mainHint.setBorder(new EmptyBorder(0,190,0,0));
+
+        JButton goodsButton = new JButton("Товари");
+        JButton groupsButton = new JButton("Групи товарів");
+        JButton searchGoodButton = new JButton("Пошук товарів");
+        JButton showDataButton = new JButton("Вивести дані");
+
+        mainFrame.add(buttonPanel);
+        buttonPanel.add(mainHint);
+        buttonPanel.add(goodsButton);
+        buttonPanel.add(groupsButton);
+        buttonPanel.add(searchGoodButton);
+        buttonPanel.add(showDataButton);
+
+        mainFrame.setVisible(true);
+        buttonPanel.setVisible(true);
     }
 }
