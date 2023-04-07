e335e4b44427963cf3eb44d68076a65ec4de9f12
TimMatthew <t.mattw.0000@gmail.com>
2023-04-07 15:13:36 +0300

    Апдейт головного меню

---
+++ src/GoodsMenu.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class GoodsMenu {
+    protected static void setGoodsMenu(){
+        JFrame goodsFrame = new JFrame();
+        goodsFrame.setSize(200,200);
+        goodsFrame.setVisible(true);
+    }
+}
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,31 +1,59 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu {
-    protected static void setMainFrame(){
-        JFrame mainFrame = new JFrame();
+    protected static JFrame mainFrame;
+    protected static void setMainMenu(){
+        mainFrame = new JFrame();
+        JPanel mainPanel = new JPanel(new BorderLayout());
         JPanel buttonPanel = new JPanel(new GridLayout(6,0));
 
         mainFrame.setSize(500,500);
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JLabel mainHint = new JLabel("STORE SIMULATOR");
-        mainHint.setBorder(new EmptyBorder(0,190,0,0));
+        mainHint.setBorder(new EmptyBorder(0,180,0,0));
 
+        // КНОПКИ ГОЛОВНОГО МЕНЮ //
         JButton goodsButton = new JButton("Товари");
         JButton groupsButton = new JButton("Групи товарів");
         JButton searchGoodButton = new JButton("Пошук товарів");
         JButton showDataButton = new JButton("Вивести дані");
 
-        mainFrame.add(buttonPanel);
+        // СТВОРЕННЯ ПАНЕЛЕЙ ДЛЯ ВІДМЕЖУВАННЯ КНОПОК ВІД СТІНОК ВІКНА //
+        JPanel eastPanel = new JPanel();
+        JPanel westPanel = new JPanel();
+
+        // ДОДАВАННЯ КНОПОК ДО ПАНЕЛІ КНОПОК //
         buttonPanel.add(mainHint);
         buttonPanel.add(goodsButton);
         buttonPanel.add(groupsButton);
         buttonPanel.add(searchGoodButton);
         buttonPanel.add(showDataButton);
 
+        goodsButton.addActionListener(goToGoods);
+
+        // ДОДАВАННЯ ОБ'ЄКТІВ ДО ГОЛОВНОЇ ПАНЕЛІ //
+        mainPanel.add(buttonPanel, BorderLayout.CENTER);
+        mainPanel.add(eastPanel, BorderLayout.EAST);
+        mainPanel.add(westPanel, BorderLayout.WEST);
+
+        mainFrame.add(mainPanel);
         mainFrame.setVisible(true);
+        mainPanel.setVisible(true);
         buttonPanel.setVisible(true);
+
     }
+
+        private static final ActionListener goToGoods = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mainFrame.setVisible(false);
+                GoodsMenu.setGoodsMenu();
+            }
+        };
+
 }
