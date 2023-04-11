c99bc6dcd5f92ab35ac842e25b6df3c8ab4cb058
TimMatthew <t.mattw.0000@gmail.com>
2023-04-11 19:53:24 +0300

    Почав робити редагування. В планах зробить панель із ГрідЛеяутом, де на кожен буде лейбл і текстове поле для змін. Також будуть кнопки ок та скасувати. Можливо тут організуємо продаж\купівлю товарів

--- src/EditGood.java
+++ src/EditGood.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 
 public class EditGood {
+    private JFrame editFrame;
     private JPanel editPanel;
     private JLabel editName;
     private JLabel editDescription;
@@ -9,5 +10,13 @@ public class EditGood {
     private JLabel editAmount;
 
     public EditGood() {
+        editFrame = new JFrame();
+        editFrame.setBounds(MainMenu.screenDimension.width/3,MainMenu.screenDimension.height/4,600,900);
+        editFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        editFrame.setVisible(true);
+    }
+
+    public void setEditMenu(){
+
     }
 }
